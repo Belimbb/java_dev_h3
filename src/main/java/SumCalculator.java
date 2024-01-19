566f558103978562c2e5922c9eda57d317959d2c
Belimbb <belovmladshui@gmail.com>
2024-01-19 22:55:31 +0200

    hotFix sum() method (now it can throw IllegalArgumentException)

--- src/main/java/SumCalculator.java
+++ src/main/java/SumCalculator.java
@@ -1,5 +1,8 @@
 public class SumCalculator {
     public int sum(int n){
+        if (n == 0){
+            throw new IllegalArgumentException();
+        }
         int counter = 0;
         int res = 0;
         while (counter<n){
